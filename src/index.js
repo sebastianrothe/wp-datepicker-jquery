e289ef5b45369e9b8d3482d46669643d0f2c3b26
Sebastian Rothe <rothe@infai.org>
2019-02-17 23:05:15 +0100

    use ready from helper

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import App from './components/app'
 import config from './config'
-import ready from './helper/ready'
+import { ready } from './helper'
 
 ready(function() {
-	const app = new App(config)
-	app.init();
-	app.renderFooterOnDatepicker();
-});
+  const app = new App(config)
+  app.init()
+  app.renderFooterOnDatepicker()
+})
