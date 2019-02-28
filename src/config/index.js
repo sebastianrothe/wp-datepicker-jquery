770a688a938733c3df5337a6de318289e3bf5fb5
Sebastian Rothe <rothe@infai.org>
2019-02-28 23:49:11 +0100

    add more config fields

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,25 @@
+// TODO: inject this object with locale support
+const tourHTMLEntities = {
+  disabled: {
+    title: 'Fully booked',
+    tooltip: 'The tour is fully booked on this date.',
+    style: 'full'
+  },
+
+  noRegularTour: {
+    title: 'Not available',
+    tooltip: 'The tour is not available on this date.',
+    style: 'not-available'
+  }
+}
+
 export default {
   locale: 'de',
   timeFormatString: '',
   classNameForInjection: '',
   testing: true,
+  allowOnlyWeekends: false,
   dataApi:
-    'hauntedleipzig.de/wordpress/wp-content/themes/hauntedleipzig/js/data.txt'
+    'hauntedleipzig.de/wordpress/wp-content/themes/hauntedleipzig/js/data.txt',
+  tourHTMLEntities: tourHTMLEntities
 }
