543b6d5f80b3714427bb55e3b260ad87578628f5
Sebastian Rothe <rothe@infai.org>
2019-03-14 00:47:18 +0100

    add convert param add more checks add tests

--- src/helper/date.js
+++ src/helper/date.js
@@ -4,18 +4,22 @@ const DATE_FRIDAY = 5
 const DATE_SATURDAY = 6
 const DATE_SUNDAY = 0
 
-const isDisabled = (date, disabledDates) => {
-  if (!disabledDates) {
+const isDisabled = (date, disabledDates, convertToLocal = false) => {
+  if (!disabledDates || disabledDates.length === 0) {
     return false
   }
 
-  const localDateString = toGermanDateString(date)
-  console.log('matching ' + localDateString + ' with ' + disabledDates)
+  const localDateString = convertToLocal ? toGermanDateString(date) : date
+  console.info('matching ' + localDateString + ' with ' + disabledDates)
   return disabledDates.indexOf(localDateString) !== -1
 }
 
 // weekend includes friday
 const isWeekend = date => {
+  if (!date || !date.getDay) {
+    return false
+  }
+
   const day = date.getDay()
   return day === DATE_FRIDAY || day === DATE_SATURDAY || day === DATE_SUNDAY
 }
