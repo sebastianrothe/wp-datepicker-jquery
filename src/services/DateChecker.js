daabc1e2842f156216b1edb0046f2b4fa291738c
Sebastian Rothe <rothe@infai.org>
2019-03-14 00:49:44 +0100

    add convert param add tests

--- src/services/DateChecker.js
+++ src/services/DateChecker.js
@@ -2,10 +2,11 @@ import { isNotWeekend, isDisabled } from '../helper/date'
 import config from '../config'
 
 export default class DateChecker {
-  constructor(disabledDates) {
+  constructor(disabledDates, convertToLocal) {
     this.disabledDates = disabledDates || []
     this.noRegularTour = config.tourHTMLEntities.noRegularTour
     this.disabledTour = config.tourHTMLEntities.disabled
+    this.convertToLocal = convertToLocal || false
   }
 
   isAvailable(date) {
@@ -13,7 +14,7 @@ export default class DateChecker {
       return [false, this.noRegularTour.style, this.noRegularTour.tooltip]
     }
 
-    if (isDisabled(date, this.disabledDates)) {
+    if (isDisabled(date, this.disabledDates, this.convertToLocal)) {
       return [false, this.disabledTour.style, this.disabledTour.tooltip]
     }
 
