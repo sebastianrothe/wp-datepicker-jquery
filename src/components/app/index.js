edba4f139d56d1c3dbbd2eb365052ecc9e2d2e90
Sebastian Rothe <rothe@infai.org>
2019-03-15 00:46:56 +0100

    extract jquery datepicker functions into component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,96 +1,39 @@
 import config from '../../config'
-import { extend, addClass } from '../../helper/polyfills'
 import DataProvider from '../../services/DataProvider'
 import DateChecker from '../../services/DateChecker'
-import { toGermanTimeString } from '../../locale/de-DE'
-import i18n from '../../i18n'
+import {
+  enableDatepicker,
+  setReadonly,
+  setRegionToGerman
+} from '../datepicker/jquery'
 
 import './style.css'
 
 export default class App {
+  constructor() {
+    this.dateChecker = new DateChecker()
+  }
+
   init() {
     console.log(config.testing)
-
-    let dateChecker = new DateChecker()
     const dataProvider = new DataProvider()
 
     const finishedLoadingDates = dates => {
-      dateChecker = new DateChecker(disabledDates)
+      this.dateChecker.setDisabledDates(disabledDates)
     }
     const disabledDates = dataProvider.fetch(finishedLoadingDates)
+  }
 
-    // get the 1st inputfield
-    const [datepickerInjectionPoint] = document.querySelectorAll(
-      config.queryElement
-    )
+  render() {
+    const [firstInput] = document.querySelectorAll(config.queryElement)
 
-    if (!datepickerInjectionPoint) {
+    if (!firstInput) {
       console.warn('Could not find injection point for the datepicker.')
       return
     }
 
-    // inject the datepicker
-    datepickerInjectionPoint.datepicker({
-      // minDate: today
-      minDate: 0,
-      // is this day already fully booked ?
-      beforeShowDay: dateChecker.isAvailable
-    })
-
-    // set datepicker input field to readonly
-    addClass(datepickerInjectionPoint, 'readonly')
-    datepickerInjectionPoint.prop('readonly', true)
-  }
-
-  // augment the jQuery Datepicker with a footer
-  renderFooterOnDatepicker() {
-    const htmlEntities = config.tourHTMLEntities
-
-    extend(jQuery.datepicker, {
-      _generateHTMLOriginal: jQuery.datepicker._generateHTML,
-
-      generateFooter: legendOptions => {
-        const lastRefreshedTime = toGermanTimeString(new Date())
-
-        let html = '<div class="ui-datepicker-footer">'
-        const items = []
-
-        // validate argument
-        legendOptions = legendOptions || []
-
-        // go through legendOptions and map properties to html code
-        const length = legendOptions.length
-        if (length) {
-          for (let i = 0; i < length; i++) {
-            // use direct assignment in this case because we're micro-optimizing.
-            const legend = legendOptions[i]
-
-            // class=color will always be assigned
-            const item = `<li><div class="color ${legend.style}" /><div>${
-              legend.title
-            }
-            </div></li>`
-            items.push(item)
-          }
-
-          html += `<ul class="legend">${items.join(
-            ''
-          )}</ul><hr class="clear" />`
-        }
-
-        html += `<div class="lastRefreshDate">${
-          i18n.strings.TEXT_LAST_REFRESHED
-        } ${lastRefreshedTime}</div></div>`
-        return html
-      },
-
-      _generateHTML: inst => {
-        const legendOptions = []
-        legendOptions.push(htmlEntities.disabled)
-
-        const footerHTML = this.generateFooter(legendOptions)
-        return this._generateHTMLOriginal(inst) + footerHTML
-      }
-    })
+    setRegionToGerman()
+    enableDatepicker(firstInput, this.dateChecker)
+    setReadonly(firstInput)
   }
 }
---
+++ src/components/datepicker/jquery.js
@@ -0,0 +1,124 @@
+import config from '../../config'
+import { extend, addClass } from '../../helper/polyfills'
+import { toGermanTimeString } from '../../locale/de-DE'
+import i18n from '../../i18n'
+
+const setRegionToGerman = () => {
+  jQuery.datepicker.regional['de'] = {
+    closeText: 'Schließen',
+    prevText: '&#x3C;Zurück',
+    nextText: 'Vor&#x3E;',
+    currentText: 'Heute',
+    monthNames: [
+      'Januar',
+      'Februar',
+      'März',
+      'April',
+      'Mai',
+      'Juni',
+      'Juli',
+      'August',
+      'September',
+      'Oktober',
+      'November',
+      'Dezember'
+    ],
+    monthNamesShort: [
+      'Jan',
+      'Feb',
+      'Mär',
+      'Apr',
+      'Mai',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Okt',
+      'Nov',
+      'Dez'
+    ],
+    dayNames: [
+      'Sonntag',
+      'Montag',
+      'Dienstag',
+      'Mittwoch',
+      'Donnerstag',
+      'Freitag',
+      'Samstag'
+    ],
+    dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+    dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+    weekHeader: 'KW',
+    dateFormat: 'dd.mm.yy',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: ''
+  }
+  jQuery.datepicker.setDefaults(jQuery.datepicker.regional['de'])
+}
+
+const enableDatepicker = (element, dateChecker) => {
+  // inject the datepicker
+  element.datepicker({
+    // minDate: today
+    minDate: 0,
+    // is this day already fully booked ?
+    beforeShowDay: dateChecker.isAvailable
+  })
+}
+
+const setReadonly = element => {
+  addClass(element, 'readonly')
+  element.prop('readonly', true)
+}
+
+const enableFooter = () => {
+  const htmlEntities = config.tourHTMLEntities
+
+  extend(jQuery.datepicker, {
+    _generateHTMLOriginal: jQuery.datepicker._generateHTML,
+
+    generateFooter: legendOptions => {
+      const lastRefreshedTime = toGermanTimeString(new Date())
+      const items = []
+      let html = '<div class="ui-datepicker-footer">'
+
+      // validate argument
+      legendOptions = legendOptions || []
+
+      // go through legendOptions and map properties to html code
+      const length = legendOptions.length
+      if (length) {
+        for (let i = 0; i < length; i++) {
+          // use direct assignment in this case because we're micro-optimizing.
+          const legend = legendOptions[i]
+
+          // class=color will always be assigned
+          const item = `<li><div class="color ${legend.style}" /><div>${
+            legend.title
+          }
+            </div></li>`
+          items.push(item)
+        }
+
+        html += `<ul class="legend">${items.join('')}</ul><hr class="clear" />`
+      }
+
+      html += `<div class="lastRefreshDate">${
+        i18n.strings.TEXT_LAST_REFRESHED
+      } ${lastRefreshedTime}</div></div>`
+      return html
+    },
+
+    _generateHTML: inst => {
+      const legendOptions = []
+      legendOptions.push(htmlEntities.disabled)
+
+      const footerHTML = this.generateFooter(legendOptions)
+      return this._generateHTMLOriginal(inst) + footerHTML
+    }
+  })
+}
+
+export { setRegionToGerman, enableDatepicker, setReadonly, enableFooter }
