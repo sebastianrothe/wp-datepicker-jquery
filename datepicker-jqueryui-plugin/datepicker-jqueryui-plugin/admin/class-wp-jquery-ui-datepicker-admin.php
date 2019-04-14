4b8c58d934b584be03fef5c01c4ea7b17c7e4b93
Sebastian Rothe <rothe@infai.org>
2019-04-14 20:29:27 +0200

    disable admin styles

--- datepicker-jqueryui-plugin/datepicker-jqueryui-plugin/admin/class-wp-jquery-ui-datepicker-admin.php
+++ datepicker-jqueryui-plugin/datepicker-jqueryui-plugin/admin/class-wp-jquery-ui-datepicker-admin.php
@@ -73,7 +73,7 @@ class Wp_Jquery_Ui_Datepicker_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-jquery-ui-datepicker-admin.css', array(), $this->version, 'all' );
+		//wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-jquery-ui-datepicker-admin.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +96,7 @@ class Wp_Jquery_Ui_Datepicker_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-jquery-ui-datepicker-admin.js', array( 'jquery' ), $this->version, false );
+		//wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-jquery-ui-datepicker-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
