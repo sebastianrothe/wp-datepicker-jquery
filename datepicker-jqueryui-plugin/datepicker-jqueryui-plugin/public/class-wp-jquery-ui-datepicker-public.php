a90b500af9345881cfdbd4f5eda22d082026a94c
Sebastian Rothe <rothe@infai.org>
2019-04-14 21:00:48 +0200

    put js in footer remove version string from url param

--- datepicker-jqueryui-plugin/datepicker-jqueryui-plugin/public/class-wp-jquery-ui-datepicker-public.php
+++ datepicker-jqueryui-plugin/datepicker-jqueryui-plugin/public/class-wp-jquery-ui-datepicker-public.php
@@ -73,9 +73,9 @@ class Wp_Jquery_Ui_Datepicker_Public {
 		 * class.
 		 */
 
-    wp_enqueue_style('jquery-ui-theme', 'https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.min.css', false, '1.12.1', 'all' );
+    wp_enqueue_style('jquery-ui-theme', 'https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.min.css', false, null, 'all' );
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/datepicker-jqueryui-plugin-'.$this->version.'.css', array('jquery-ui-theme'), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/datepicker-jqueryui-plugin-'.$this->version.'.css', array('jquery-ui-theme'), null, 'all' );
 
 	}
 
@@ -98,12 +98,12 @@ class Wp_Jquery_Ui_Datepicker_Public {
 		 * class.
 		 */
 
-    wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.4.0.min.js', false, '3.4.0', false );
+    wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.4.0.min.js', false, null, true );
 
-    wp_enqueue_script('jquery-ui', 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js', ['jquery'], '3.4.0', false );
+    wp_enqueue_script('jquery-ui', 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js', ['jquery'], null, true );
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/datepicker-jqueryui-plugin-'.$this->version.'.js', [
-    'jquery-ui'], $this->version, false );
+    'jquery-ui'], null, true );
 
 	}
 
