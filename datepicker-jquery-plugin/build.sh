cbfe589ca8213d5f553b10df0f9a72fa31914f8e
Sebastian Rothe <rothe@infai.org>
2019-04-14 20:20:55 +0200

    use variables

--- datepicker-jquery-plugin/build.sh
+++ datepicker-jquery-plugin/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-zip -r -9 dist/wp-jquery-ui-datepicker-MASTER.zip wp-jquery-ui-datepicker \
+VERSION=${1:MASTER}
+NAME=datepicker-jquery-plugin
+OUTPUT_PATH=dist
+
+mkdir -p $OUTPUT_PATH
+
+zip -r -9 $OUTPUT_PATH/$NAME-$VERSION.zip $NAME \
     -x */.DS_Store \
     -x */.git \
     -x */.svn \
