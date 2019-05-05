0e73c64c25574a3e1fc980c3781c2d5f4cceee81
Sebastian Rothe <rothe@infai.org>
2019-05-05 13:25:06 +0200

    fix input injection

--- src/config/index.js
+++ src/config/index.js
@@ -6,5 +6,5 @@ export default {
     '//gruseltour-berlin.de/wordpress/wp-content/themes/gruseltour-berlin-eighteen/data/data.txt',
   dataApiDev: 'data/data.txt',
   queryElement:
-    '.wpforms-container .wpforms-field-container .wpforms-field.wpforms-field-text.datepicker'
+    '.wpforms-container .wpforms-field-container .wpforms-field.datepicker input'
 }
