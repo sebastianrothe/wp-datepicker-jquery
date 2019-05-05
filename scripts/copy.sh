468b6cfe846a2cc51f196e632b51b9dc4ea529bf
Sebastian Rothe <rothe@infai.org>
2019-05-05 12:27:25 +0200

    use jquery ui custom

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,11 +2,13 @@
 VERSION=${1:-develop}
 NAME=datepicker-jqueryui-plugin
 OUTPUT_PATH="$NAME/$NAME/public"
-BUNDLE_PATH=bundle
+BUNDLE_PATH=${2:-bundle}
 
 copyAndRename() {
   local filetype=$1
   ls -1 $BUNDLE_PATH/src.*.$filetype | sed "p;s%$BUNDLE_PATH/src\..*\.$filetype$%$OUTPUT_PATH/$filetype/$NAME-$VERSION\.$filetype%g" | xargs -n2 cp
+
+  ls -1 $BUNDLE_PATH/jquery-ui.*.$filetype | sed "p;s%$BUNDLE_PATH/jquery-ui\..*\.$filetype$%$OUTPUT_PATH/$filetype/jquery-ui.custom.min\.$filetype%g" | xargs -n2 cp
 }
 
 main() {
