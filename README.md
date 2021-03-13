f7ca5c7e0f8d3ba640bd450e9cc1d71421a3f878
Sebastian Rothe <rothe@infai.org>
2021-03-13 23:50:32 +0100

    replace jest with mocha and chai running from web-test-runner

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # wp-datepicker-jqueryui
+
 A Datepicker for Wordpress with JQuery UI
 
-## TODO
-- read options from config file on server
-- - generate options file from wordpress plugin settings
-- add noscript
-- hide disabled dates before today
-- replace floats with flexbox
 
-- use jquery slim - not possible with datepicker
+## TODO
+
+* [ ] replace Parcel with Snowpack
+* [ ] replace xhr with fetch
+* [ ] read options from config file on server
+  * [ ] generate options file from wordpress plugin settings
+* [ ] add noscript
+* [ ] hide disabled dates before today
+* [ ] replace floats with flexbox
+
+* [/] use jquery slim - not possible with datepicker
--- src/helper/date.test.js
+++ src/helper/date.test.js
@@ -1,59 +1,61 @@
+import { expect } from '@esm-bundle/chai'
+
 import { isWeekend, isNotWeekend } from './date'
 
 describe('DateHelper', () => {
   describe('has functions for weekends', () => {
     it('should return true, for weekends', () => {
       const saturday = new Date(2019, 2, 9)
-      expect(saturday.getDay()).toBe(6)
-      expect(isWeekend(saturday)).toBeTruthy()
-      expect(isNotWeekend(saturday)).toBeFalsy()
+      expect(saturday.getDay()).to.equal(6)
+      expect(isWeekend(saturday)).to.be.ok
+      expect(isNotWeekend(saturday)).to.be.not.ok
 
       const sunday = new Date(2019, 2, 10)
-      expect(sunday.getDay()).toBe(0)
-      expect(isWeekend(sunday)).toBeTruthy()
-      expect(isNotWeekend(sunday)).toBeFalsy()
+      expect(sunday.getDay()).to.equal(0)
+      expect(isWeekend(sunday)).to.be.ok
+      expect(isNotWeekend(sunday)).to.be.not.ok
     })
 
     it('should return true, for fridays', () => {
       const friday = new Date(2019, 2, 8)
-      expect(friday.getDay()).toBe(5)
-      expect(isWeekend(friday)).toBeTruthy()
-      expect(isNotWeekend(friday)).toBeFalsy()
+      expect(friday.getDay()).to.equal(5)
+      expect(isWeekend(friday)).to.be.ok
+      expect(isNotWeekend(friday)).to.be.not.ok
     })
 
     it('should return false, for weekdays', () => {
       const monday = new Date(2019, 2, 4)
-      expect(monday.getDay()).toBe(1)
-      expect(isWeekend(monday)).toBeFalsy()
-      expect(isNotWeekend(monday)).toBeTruthy()
+      expect(monday.getDay()).to.equal(1)
+      expect(isWeekend(monday)).to.be.not.ok
+      expect(isNotWeekend(monday)).to.be.ok
 
       const thursay = new Date(2019, 2, 5)
-      expect(thursay.getDay()).toBe(2)
-      expect(isWeekend(thursay)).toBeFalsy()
-      expect(isNotWeekend(thursay)).toBeTruthy()
+      expect(thursay.getDay()).to.equal(2)
+      expect(isWeekend(thursay)).to.be.not.ok
+      expect(isNotWeekend(thursay)).to.be.ok
 
       const wednesday = new Date(2019, 2, 6)
-      expect(wednesday.getDay()).toBe(3)
-      expect(isWeekend(wednesday)).toBeFalsy()
-      expect(isNotWeekend(wednesday)).toBeTruthy()
+      expect(wednesday.getDay()).to.equal(3)
+      expect(isWeekend(wednesday)).to.be.not.ok
+      expect(isNotWeekend(wednesday)).to.be.ok
 
       const thursday = new Date(2019, 2, 7)
-      expect(thursday.getDay()).toBe(4)
-      expect(isWeekend(thursday)).toBeFalsy()
-      expect(isNotWeekend(thursday)).toBeTruthy()
+      expect(thursday.getDay()).to.equal(4)
+      expect(isWeekend(thursday)).to.be.not.ok
+      expect(isNotWeekend(thursday)).to.be.ok
     })
 
     it('should return false for german strings', () => {
       const friday = '08.03.2019'
-      expect(isWeekend(friday)).toBeFalsy()
-      expect(isNotWeekend(friday)).toBeTruthy()
+      expect(isWeekend(friday)).to.be.not.ok
+      expect(isNotWeekend(friday)).to.be.ok
     })
 
     it('should return true for english strings', () => {
       const friday = new Date('2019-03-08')
-      expect(friday.getDay()).toBe(5)
-      expect(isWeekend(friday)).toBeTruthy()
-      expect(isNotWeekend(friday)).toBeFalsy()
+      expect(friday.getDay()).to.equal(5)
+      expect(isWeekend(friday)).to.be.ok
+      expect(isNotWeekend(friday)).to.be.not.ok
     })
   })
 })
