595524dedd62e7993273699351beb2bbd5134624
Sebastian Rothe <rothe@infai.org>
2021-03-13 01:02:35 +0100

    load dataApi uri from env

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,7 @@ export default {
   locale: 'de',
   timeFormatString: 'dd.mm.yy',
   allowOnlyWeekends: false,
-  dataApi:
-    '//gruseltour-berlin.de/wordpress/wp-content/themes/gruseltour-berlin-eighteen/data/data.txt',
-  dataApiDev: 'data/data.txt',
+  dataApi: import.meta.env.SNOWPACK_PUBLIC_DATA_URL,
   queryElement:
     '.wpforms-container .wpforms-field-container .wpforms-field.datepicker input'
 }
