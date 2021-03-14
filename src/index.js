32bd5bf8f98e9c6bb7b20a580509aee13fe751fe
Sebastian Rothe <rothe@infai.org>
2021-03-14 23:37:26 +0100

    change comment

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ ready(() => {
   app.render()
 })
 
-
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+// Hot Module Replacement https://www.snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
