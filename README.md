503f29131014edd60dd1f583130d65621090c917
Sebastian Rothe <basti@lifeinprogress.de>
2021-03-26 10:49:49 +0100

    merge

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # wp-datepicker-jqueryui
 
-A Datepicker for Wordpress with JQuery UI
+A Datepicker for Wordpress with JQuery UI, custom style and disabled dates.
 
 
 ## Development
@@ -20,14 +20,19 @@ A Datepicker for Wordpress with JQuery UI
 
 ## TODO
 
-* [x] replace Parcel with Snowpack
-* [x] Prettier
-  * [x] (optional, because they are slower than Prettier) Eslint, Stylelint
+
 * [ ] replace jquery with lit-html/svelte
 * [ ] replace floats with flexbox
 * [ ] add noscript text
+* [ ] replace snowpack with vitejs
 * [ ] read options from config file on server
   * [ ] generate options file from wordpress plugin settings
 * [ ] hide disabled dates before today
 * [x] replace xhr with fetch - IE11 needs two Polyfills (fetch and Promises) then
 * [x] use jquery slim - not possible with datepicker
+* [x] add linter
+* [x] add prettier
+* [x] add postcss
+* [x] replace Parcel with Snowpack
+* [x] add Prettier
+  * [x] (optional, because they are slower than Prettier) add Eslint, Stylelint
--- snowpack.config.js
+++ snowpack.config.js
@@ -1,33 +1,38 @@
 // Snowpack Configuration File
 // See all supported options: https://www.snowpack.dev/reference/configuration
 
+const ecmaVersion = "es2018";
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
     "public": { url: "/", static: true, resolve: false },
     "src": "/dist",
   },
-  plugins: ["@snowpack/plugin-dotenv"],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
+  buildOptions: {
+    minify: false,
+  },
+  plugins: [
+    "@snowpack/plugin-dotenv",
+  "@snowpack/plugin-postcss",
+  ['snowpack-plugin-terser', {
+    terserOptions: {
+      compress: {
+        ecma: ecmaVersion
+      },
+      format: {
+        ecma: ecmaVersion
+      }
+    },
+  }]
+],
   optimize: {
     entrypoints: [
       "src/index.js",
     ],
     bundle: true,
-    minify: true,
+    minify: false,
     treeshake: true,
-    target: 'es2018',
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+    target: ecmaVersion,
+  }
 };
