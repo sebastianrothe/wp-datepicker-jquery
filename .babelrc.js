b4e5706a8b956e72ef1743ecf14392bf0fde31e3
Sebastian Rothe <rothe@infai.org>
2021-03-26 10:25:29 +0100

    remove unused babel

--- .babelrc.js
+++
@@ -1,9 +0,0 @@
-var env = process.env.BABEL_ENV || process.env.NODE_ENV;
-
-// inline plugin
-module.exports = {
-  plugins: [
-    env === 'production' && ["transform-remove-console", { "exclude": [ "error", "warn"] }],
-    env === 'test' && "transform-es2015-modules-commonjs"
-  ].filter(Boolean)
-};
