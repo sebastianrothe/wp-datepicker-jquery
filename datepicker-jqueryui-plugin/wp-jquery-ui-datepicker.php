117004ac965bfa416d81d280744a007593a6026c
Sebastian Rothe <basti@lifeinprogress.de>
2021-05-23 21:00:53 +0200

    update version to v1.1.0

--- datepicker-jqueryui-plugin/wp-jquery-ui-datepicker.php
+++ datepicker-jqueryui-plugin/wp-jquery-ui-datepicker.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WP JQueryUI Datepicker
  * Plugin URI:        https://github.com/sebastianrothe
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           1.1.0
  * Author:            Mysterium Tremendum GbR
  * Author URI:        https://gruseltour-leipzig.de
  * License:           GPL-2.0+
