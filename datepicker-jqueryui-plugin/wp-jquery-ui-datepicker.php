dae576495bf9fae1fa542c27bb01f010f11fba8c
Sebastian Rothe <basti@lifeinprogress.de>
2021-11-26 12:12:56 +0100

    bump version

--- datepicker-jqueryui-plugin/wp-jquery-ui-datepicker.php
+++ datepicker-jqueryui-plugin/wp-jquery-ui-datepicker.php
@@ -16,11 +16,9 @@
  * Plugin Name:       WP JQueryUI Datepicker
  * Plugin URI:        https://github.com/sebastianrothe
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.1.0
+ * Version:           1.2.0
  * Author:            Mysterium Tremendum GbR
  * Author URI:        https://gruseltour-leipzig.de
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wp-jquery-ui-datepicker
  * Domain Path:       /languages
  */
@@ -35,7 +33,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WP_JQUERY_UI_DATEPICKER_VERSION', '1.1.0' );
+define( 'WP_JQUERY_UI_DATEPICKER_VERSION', '1.2.0' );
 
 /**
  * The code that runs during plugin activation.
