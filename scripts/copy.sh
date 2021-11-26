a6b25d4d9349419b63eb0d772e8a05a5f0f87c44
Sebastian Rothe <basti@lifeinprogress.de>
2021-11-26 16:15:38 +0100

    add version to copy script

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 VERSION=${1:-develop}
+JQUERYUI_VERSION="1.13.0.custom-smoothness"
 NAME=datepicker-jqueryui-plugin
 OUTPUT_PATH="$NAME/public"
 BUNDLE_PATH=${2:-build}
@@ -12,7 +13,7 @@ copyAndRename() {
   ls -1 $BUNDLE_PATH/dist/index.$filetype | sed "p;s%$BUNDLE_PATH/dist/\index\.$filetype$%$OUTPUT_PATH/$folder/$NAME-$VERSION\.$filetype%g" | xargs -n2 cp
 
   # copy and rename jquery-custom files
-  ls -1 $BUNDLE_PATH/js/jquery-ui-1.12.1.custom-smoothness/jquery-ui.*.$filetype | sed "p;s%$BUNDLE_PATH/js/jquery-ui-1.12.1.custom-smoothness/jquery-ui\..*\.$filetype$%$OUTPUT_PATH/$folder/jquery-ui-custom.min\.$filetype%g" | xargs -n2 cp
+  ls -1 $BUNDLE_PATH/js/jquery-ui-$JQUERYUI_VERSION/jquery-ui.*.$filetype | sed "p;s%$BUNDLE_PATH/js/jquery-ui-$JQUERYUI_VERSION/jquery-ui\..*\.$filetype$%$OUTPUT_PATH/$folder/jquery-ui-custom.min\.$filetype%g" | xargs -n2 cp
 }
 
 copyAndRenameWithCompression() {
@@ -20,6 +21,7 @@ copyAndRenameWithCompression() {
   local filetype=$1
 
   copyAndRename $folder $filetype
+  # these are not generated, so we dont copy them
   #copyAndRename $folder "$filetype.br"
   #copyAndRename $folder "$filetype.gz"
 }
