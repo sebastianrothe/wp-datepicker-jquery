368916655dfb35ecbbf6398fe127553afdfbcab3
Sebastian Rothe <basti@lifeinprogress.de>
2022-06-21 00:50:42 +0200

    use recommended env var usage

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,12 @@
+// eslint-reason otherwise SNOWPACK_ENV doesn't work. See https://github.com/FredKSchott/snowpack/issues/3621
+// eslint-disable-next-line no-unused-expressions
+import.meta.hot
+
 export default {
   locale: "de",
   timeFormatString: "dd.mm.yy",
   allowOnlyWeekends: false,
-  dataApi: import.meta.env.SNOWPACK_PUBLIC_DATA_URL,
+  dataApi: __SNOWPACK_ENV__.SNOWPACK_PUBLIC_DATA_URL,
   queryElement:
     ".wpforms-container .wpforms-field-container .wpforms-field.datepicker input",
 }
